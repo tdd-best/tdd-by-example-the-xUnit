e08b68cc220c665f8a40feeccdf447b5333d31cc
Joey Chen <jay.hatelove@gmail.com>
2020-10-05 18:09:30 +0800

    ch18-10: pull member __init__ up, and only put self.name setting to superclass; and made WasRun override __init__ for setting wasRun as None

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 class TestCase:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
-        self.name = name
+        TestCase.__init__(self, name)
 
     def run(self):
         method = getattr(self, self.name)
