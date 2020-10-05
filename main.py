9d363799667f59cc2668e1517a9fdbc736d3881a
Joey Chen <jay.hatelove@gmail.com>
2020-10-05 17:48:50 +0800

    ch18-8: use getattr() to invoke specific function of self, like testMethod()

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
     def testMethod(self):
         self.wasRun = 1
-
-    def run(self):
-        self.testMethod()
 
 
 if __name__ == '__main__':
