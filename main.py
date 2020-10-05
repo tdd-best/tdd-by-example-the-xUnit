16164285bc0ca83b74e5bd77bd282318065c7bdc
Joey Chen <jay.hatelove@gmail.com>
2020-10-05 18:17:12 +0800

    ch18-12: create a test class "TestCaseTest" to test our WasRun

--- main.py
+++ main.py
@@ -16,8 +16,17 @@ class WasRun(TestCase):
         self.wasRun = 1
 
 
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert (not test.wasRun)
+        test.run()
+        assert (test.wasRun)
+
+
 if __name__ == '__main__':
-    test = WasRun("testMethod")
-    print test.wasRun
-    test.run()
-    print test.wasRun
+    TestCaseTest("testRunning").run()
+    # test = WasRun("testMethod")
+    # print test.wasRun
+    # test.run()
+    # print test.wasRun
