8feaae9109cf66380d152b0d43c9a73211f0f6d7
Joey Chen <jay.hatelove@gmail.com>
2020-10-07 16:08:55 +0800

    ch20-5: remove method testRunning() and rename testSetUp() to testTemplateMethod()

--- main.py
+++ main.py
@@ -31,14 +31,10 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
-
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert ("setUp testMethod " == self.test.log)
 
 
 if __name__ == '__main__':
-    TestCaseTest("testSetUp").run()
+    TestCaseTest("testTemplateMethod").run()
